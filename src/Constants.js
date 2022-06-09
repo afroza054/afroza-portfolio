1e0aeade704e45c45a00f15919af8bc09376ce61
afroza054 <afrozajabin5454@gmail.com>
2022-06-09 23:59:08 +0600

    first commit

---
+++ src/Constants.js
@@ -0,0 +1,62 @@
+import COAST from './Assets/Projects/top11.png';
+import COSMOS from './Assets/Projects/project1.png';
+import TRAVELWELL from './Assets/Projects/project2.png';
+
+export const PROJECTS = [
+  {
+    image: COSMOS,
+    name: 'Tools Management',
+    technologyUsed:
+      'HTML, CSS, ReactJs, NodeJs, ExpressJs, Mongodb, Firebase, Axios',
+    description:
+      'User roles are defined in tools website and added tools with minimum quantity',
+    url: 'https://assignment12client.web.app/',
+  },
+  {
+    image: TRAVELWELL,
+    name: 'Student Earning',
+    technologyUsed: 'HTML, CSS, JavaScript ES6',
+    description:
+      'Student earning is a app where student can calculate expanse, saving and the remaining',
+    url: 'https://lucid-goldstine-f2d9ce.netlify.app/',
+  },
+  {
+    image: COAST,
+    name: 'Influencer',
+    technologyUsed: 'HTML, CSS',
+    description:
+      'Different types of camera Image',
+    url: 'https://afroza054.github.io/A_Assignment_2_D/',
+  },
+];
+
+export const SKILLS = [
+  { name: 'ReactJs', initialRating: 4 },
+  // { name: 'React-Native', initialRating: 4 },
+  { name: 'JavaScript', initialRating: 4 },
+  // { name: 'Redux', initialRating: 4 },
+  // { name: 'Shopify/Reactjs', initialRating: 4 },
+  { name: 'Nodejs', initialRating: 4 },
+  { name: 'Expressjs', initialRating: 4 },
+  { name: 'MongoDB', initialRating: 4 },
+  // { name: 'Koajs', initialRating: 3 },
+  { name: 'Html', initialRating: 4 },
+  { name: 'CSS', initialRating: 4 },
+  // { name: 'Unit-Testing(Jest)', initialRating: 3 },
+  // { name: 'TypeScript', initialRating: 4 },
+  // { name: 'Graphql', initialRating: 4 },
+  // { name: 'Apollo', initialRating: 4 },
+  // { name: 'AntD | ReactStrap | Material UI', initialRating: 4 },
+  // { name: 'Shopify Polaris', initialRating: 4 },
+  { name: 'Git', initialRating: 4 },
+];
+
+export const TOOLS = [
+  'Visual Studio Code',
+  'Git',
+  'Chrome DevTools',
+  'Npm (Node Package Manager)',
+  'Heroku',
+  'Firebase',
+  'Netlify',
+];
